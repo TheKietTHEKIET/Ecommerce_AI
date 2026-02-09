f4fdb66993b9adfdc563000902704dbc34468f31
TheKietTHEKIET <hoangthekiet1412@gmail.com>
2026-02-09 23:26:09 +0700

    setup project structure, schema

---
+++ app/(app)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* Featured products carousel */}
+
+      {/* Page banner */}
+
+      {/* category tiles */}
+
+      {/* products section */}
+    </div>
+  );
+}
